bb2c523210a5503a01064431a84eb6243345529f
finansial148 <mandirifinansial13@gmail.com>
2026-02-27 21:57:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const namaCache = 'editor-v7'; // Ganti v1 jadi v2 biar browser sadar ada update
+const namaCache = 'editor-v8'; // Ganti v1 jadi v2 biar browser sadar ada update
 
 const fileWajib = [
   './',
